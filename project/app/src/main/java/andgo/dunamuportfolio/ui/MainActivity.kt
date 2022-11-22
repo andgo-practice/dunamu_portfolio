6eefefdfe5eb611cf2c875fc889592c8c28f40b2
andgo2 <andgo@andgotips.com>
2022-11-22 22:49:59 +0900

    [#2] CoinPriceFragment 제거

--- project/app/src/main/java/andgo/dunamuportfolio/ui/MainActivity.kt
+++ project/app/src/main/java/andgo/dunamuportfolio/ui/MainActivity.kt
@@ -1,16 +1,49 @@
 package andgo.dunamuportfolio.ui
 
-import andgo.dunamuportfolio.R
+import andgo.dunamuportfolio.domain.model.CoinPriceUnit
+import andgo.dunamuportfolio.domain.model.mockPriceList
+import andgo.dunamuportfolio.ui.price.CoinPriceViewModel
+import andgo.dunamuportfolio.ui.theme.DunamuPortfolioTheme
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : FragmentActivity() {
+class MainActivity : ComponentActivity() {
+    private val viewModel: CoinPriceViewModel by viewModels()
 
-    // TODO 추후 xml -> compose viewpager로 전환 예정
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewModel.setUnit(CoinPriceUnit.KRW)
+        setContent {
+            DunamuPortfolioTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CoinPriceList(
+                        unit = CoinPriceUnit.KRW,
+                        coinPriceList = viewModel.uiState.collectAsState().value.coinPriceList
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainActivityPreview() {
+    DunamuPortfolioTheme {
+        CoinPriceList(unit = CoinPriceUnit.KRW, coinPriceList = mockPriceList)
     }
 }
